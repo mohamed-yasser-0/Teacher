b76c948eebf3ad5f51a72333b90f293fca2a1c1a
mojsjss <m.yasser.dev@gmail.com>
2026-04-23 14:02:16 +0200

    first commit

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,9 +3,6 @@ const { register, login } = require("../controllers/Users.controller.js")
 const router = express.Router()
 
 
-router.route("/register")
-    .post(register)
-router.route("/login")
-    .post(login)
-
+router.post("/register", register);
+router.post("/login", login);
 module.exports = router
